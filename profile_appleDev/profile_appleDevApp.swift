4e29b211d20aa5aa175dc18dc85f9345b750a0a6
shoug <sho0oshkh@gmail.com>
2023-10-08 10:12:41 +0300

    Initial Commit

---
+++ profile_appleDev/profile_appleDevApp.swift
@@ -0,0 +1,17 @@
+//
+//  profile_appleDevApp.swift
+//  profile_appleDev
+//
+//  Created by shoug on 23/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct profile_appleDevApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
